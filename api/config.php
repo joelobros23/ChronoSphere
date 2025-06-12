3ba4e1c09959cac7fb6e8bdbd6126ebd6c04da01
joelobros23 <laberintoj8@gmail.com>
2025-06-13 04:42:46 +0800

    feat: Configure database connection in api/config.php.

--- api/config.php
+++ api/config.php
@@ -1,3 +1,15 @@
 <?php
-// PHP code will be generated here.
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "chronosphere";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully"; //remove after testing
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
